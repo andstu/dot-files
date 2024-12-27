5a81ab4b3c4f993eb689f77ab82f77cc9dbd6764
andstu <andstu.dev@gmail.com>
2024-12-27 16:41:46 -0500

    Mostly LSP

--- nvim/.config/nvim/lua/andstu/init.lua
+++ nvim/.config/nvim/lua/andstu/init.lua
@@ -6,4 +6,6 @@ require("kanagawa").load("dragon")
 
 vim.cmd("set relativenumber")
 vim.cmd("set number")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
 
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,205 @@
 return {
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'hrsh7th/cmp-path'},
-	{"williamboman/mason.nvim"},
-	{"williamboman/mason-lspconfig.nvim"},
-	{"L3MON4D3/LuaSnip"},
-	{'saadparwaiz1/cmp_luasnip'},
-	{'rafamadriz/friendly-snippets'},
+{
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = 'rafamadriz/friendly-snippets',
+
+  -- use a release tag to download pre-built binaries
+  version = '*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- See the full "keymap" documentation for information on defining your own keymap.
+    keymap = { preset = 'enter' },
+
+    appearance = {
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+    completion = {
+      accept = {
+        -- experimental auto-brackets support
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      menu = {
+        draw = {
+          treesitter = { "lsp" },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+    },
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+  },
+  opts_extend = { "sources.default" }
+},
+-- LSP servers and clients communicate which features they support through "capabilities".
+--  By default, Neovim supports a subset of the LSP specification.
+--  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
+--  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+--
+-- This can vary by config, but in general for nvim-lspconfig:
+{'neovim/nvim-lspconfig',
+  
+},
+{
+  "nvim-treesitter/nvim-treesitter",
+  version = false, -- last release is way too old and doesn't work on Windows
+  build = ":TSUpdate",
+  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+  init = function(plugin)
+    -- perf: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- this is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    -- luckily, the only things that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
+  end,
+  keys = {
+    { "<c-space>", desc = "increment selection" },
+    { "<bs>", desc = "decrement selection", mode = "x" },
+  },
+  opts_extend = { "ensure_installed" },
+  ---@type tsconfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<c-space>",
+        node_incremental = "<c-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+      },
+    },
+  },
+  ---@param opts tsconfig
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+},
+{
+  "williamboman/mason.nvim",
+  dependencies = { 'saghen/blink.cmp', },
+  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+  build = ":MasonUpdate",
+  opts_extend = { "ensure_installed" },
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+	  "texlab",
+	  "marksman",
+	  "pyright",
+	  "tflint",
+	  "gopls"
+    },
+  },
+  ---@param opts masonsettings | {ensure_installed: string[]}
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mr = require("mason-registry")
+    mr:on("package:install:success", function()
+      vim.defer_fn(function()
+        -- trigger filetype event to possibly load this newly installed lsp server
+        require("lazy.core.handler.event").trigger({
+          event = "FileType",
+          buf = vim.api.nvim_get_current_buf(),
+        })
+      end, 100)
+    end)
+
+    mr.refresh(function()
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end)
+  end,
+},
+	{ "williamboman/mason-lspconfig.nvim", 
+	  dependencies = { 'saghen/blink.cmp', 'williamboman/mason.nvim' },
+	  config = function()
+			   end,
+	  handler = function(server_name)
+					printf('setting up %s', server_name)
+					require('lspconfig')[server_name].setup({})
+				end,
+	},
+{
+  "echasnovski/mini.comment",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      custom_commentstring = function()
+        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+      end,
+    },
+  },
+},
+{
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  lazy = true,
+  opts = {
+    enable_autocmd = false,
+  },
+},
 }
+
